2df0ad3fd299dd2425a33d754ef696ae81dcdb99
Rafael Antunes <rafael.uminho@hotmail.com>
2016-10-07 13:42:46 +0100

    Added light sensor interface plus other smal test itens

--- app/src/main/java/com/example/rafael/lightsensor/MainActivity.java
+++ app/src/main/java/com/example/rafael/lightsensor/MainActivity.java
@@ -2,18 +2,82 @@ package com.example.rafael.lightsensor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.View;
 import android.widget.TextView;
+import android.hardware.SensorManager;
+import android.hardware.Sensor;
+import android.content.Context;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.content.Intent;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mLight, mAccelerometer;
+    private TextView tv_lumens;
+    private TextView tv_lightAcc;
+
+
+    public final static String EXTRA_MESSAGE = "com.example.rafael.lightsensor.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        this.tv_lumens = (TextView) findViewById(R.id.t_lumens);
+        this.tv_lightAcc = (TextView) findViewById(R.id.t_lightAcc);
+
+        //Keep the screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+        TextView tv2 = (TextView) findViewById(R.id.t_helloC);
+        tv2.setText("Hello From Java!!");
+
+        // Get an instance of the sensor service, and use that to get an instance of
+        // a particular sensor.
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+        switch(sensor.getType()){
+            case(Sensor.TYPE_LIGHT):
+                tv_lightAcc.setText("Accuracy Changed to " + accuracy);
+                break;
+            default:
+        }
+
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        //tv_lumens.setText("Sensor Changed !!!!!!!!");
+        float lumens = event.values[0];
+        // Do something with this sensor data.
+        tv_lumens.setText(Float.toString(lumens));
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 
     /**
@@ -25,5 +89,14 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
