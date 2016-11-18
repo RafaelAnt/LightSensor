40abda5a687dc6360fe976acec21b2ec9f988ecb
Rafael Antunes <rafael.uminho@hotmail.com>
2016-11-18 16:59:40 +0000

    Toolbar!

--- app/src/main/java/com/example/rafael/lightsensor/DisplayMessageActivity.java
+++ app/src/main/java/com/example/rafael/lightsensor/DisplayMessageActivity.java
@@ -3,6 +3,8 @@ package com.example.rafael.lightsensor;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -21,5 +23,16 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
     }
 }
