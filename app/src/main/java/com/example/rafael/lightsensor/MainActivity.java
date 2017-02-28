f04e1f60b7572091dafd3af61e17022166288652
Rafael Antunes <rafael.uminho@hotmail.com>
2017-02-28 19:53:57 +0000

    Update

--- app/src/main/java/com/example/rafael/lightsensor/MainActivity.java
+++ app/src/main/java/com/example/rafael/lightsensor/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.rafael.lightsensor;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 import android.view.View;
 import android.widget.TextView;
@@ -21,6 +28,13 @@ import android.content.Intent;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import static android.Manifest.permission.READ_CONTACTS;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -32,10 +46,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView tv_latitudeGPS, tv_longitudeGPS, tv_alturaGPS;
     private TextView tv_latitudeNet, tv_longitudeNet, tv_alturaNet;
     private TextView tv_permission;
+    private TextView tv_cidade;
+    private TextView tv_weather;
     private LocationManager locationManager;
     private LocationListener locationListenerGPS, locationListenerNet;
     private final int MY_PERMISSIONS_REQUEST_MULTIPLE_LOCATION = 600;
 
+    private Location locationGPS;
     public final static String EXTRA_MESSAGE = "com.example.rafael.lightsensor.MESSAGE";
 
     @Override
@@ -58,6 +75,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         this.tv_longitudeNet = (TextView) findViewById(R.id.t_longitudeNet);
         this.tv_alturaNet = (TextView) findViewById(R.id.t_alturaNet);
         this.tv_permission = (TextView) findViewById(R.id.t_permission);
+        this.tv_cidade = (TextView) findViewById(R.id.tv_cidade_edit);
+        this.tv_weather = (TextView) findViewById(R.id.tv_weather_edit);
 
         //Keep the screen on
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -75,10 +94,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         // Location
+        locationGPS = null;
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationListenerGPS = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
+                if(locationGPS == null) locationGPS = location;
                 tv_latitudeGPS.setText(Double.toString(location.getLatitude()));
                 tv_longitudeGPS.setText(Double.toString(location.getLongitude()));
                 tv_alturaGPS.setText(Double.toString(location.getAltitude()));
@@ -103,6 +124,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         locationListenerNet = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
+                if(locationGPS == null) locationGPS = location;
                 tv_latitudeNet.setText(Double.toString(location.getLatitude()));
                 tv_longitudeNet.setText(Double.toString(location.getLongitude()));
                 tv_alturaNet.setText(Double.toString(location.getAltitude()));
@@ -219,20 +241,119 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         System.loadLibrary("native-lib");
     }
 
-    /**
-     * Called when the user clicks the Send button
-     */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
 
     public void touchScreen (View view){
         Intent intent = new Intent(this, TouchScreenActivity.class);
         startActivity(intent);
 
     }
+
+    public void getCity (View view){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        AlertDialog alertDialog;
+
+        String city;
+        String countryCode;
+
+        if(locationGPS == null){
+
+            // set dialog message
+            alertDialogBuilder.setMessage("Location not available...");
+
+            // create alert dialog
+            alertDialog = alertDialogBuilder.create();
+            // show it
+            alertDialog.show();
+
+            return;
+        }
+
+        Geocoder gcd = new Geocoder(MainActivity.this, Locale.ENGLISH);
+        List<Address> addresses = null;
+        try {
+            Log.d("location",locationGPS.toString());
+            addresses = gcd.getFromLocation(locationGPS.getLatitude(), locationGPS.getLongitude(), 1);
+        } catch (IOException e) {
+            Log.e("Main Activity","error noGetFromLocation");
+            e.printStackTrace();
+        }
+        if (addresses != null && addresses.size() > 0){
+            //System.out.println();
+            // set dialog message
+            city = addresses.get(0).getSubAdminArea();
+            countryCode=addresses.get(0).getCountryCode();
+            alertDialogBuilder.setMessage("Location: " + city + ", " + countryCode);
+            Log.d("Address","The adress is: " + addresses.get(0));
+
+            // create alert dialog
+            alertDialog = alertDialogBuilder.create();
+            // show it
+            alertDialog.show();
+
+            JSONWeatherTask task = new JSONWeatherTask();
+            task.execute(new String[]{city+","+countryCode});
+
+
+        }else{
+            // set dialog message
+            alertDialogBuilder.setMessage("Location not found...");
+            // create alert dialog
+            alertDialog = alertDialogBuilder.create();
+            // show it
+            alertDialog.show();
+        }
+    }
+
+
+
+
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            Weather weather = new Weather();
+            String data = ( (new WeatherHttpClient()).getWeatherData(params[0]));
+
+            try {
+                weather = JSONWeatherParser.getWeather(data);
+
+                // Let's retrieve the icon
+                //weather.iconData = ( (new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return weather;
+
+        }
+
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+
+            if (weather.iconData != null && weather.iconData.length > 0) {
+                Bitmap img = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length);
+                //imgView.setImageBitmap(img);
+            }
+
+            //Log.d("Result",""+weather.currentCondition.getCondition());
+            tv_cidade.setText(weather.location.getCity() + "," + weather.location.getCountry());
+            tv_weather.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
+            /*temp.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "�C");
+            hum.setText("" + weather.currentCondition.getHumidity() + "%");
+            press.setText("" + weather.currentCondition.getPressure() + " hPa");
+            windSpeed.setText("" + weather.wind.getSpeed() + " mps");
+            windDeg.setText("" + weather.wind.getDeg() + "�");*/
+
+        }
+
+
+
+
+
+
+
+    }
+
 }
